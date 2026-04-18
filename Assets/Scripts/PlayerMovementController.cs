879545b464e22b8997f53262a48d2e8adb70fe18
Samantha Difeo <sdifeo1@gmail.com>
2026-04-18 12:20:29 -0400

    Camera first person cinemachine

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -7,18 +7,11 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField]
     private float speed = 10f;
 
-    [Header("Look Params")] [SerializeField]
-    private float sensitivity = 0.5f;
-    private float vertLimit = 90f;
-    
     [Header("Unity Game Objects in the Player GO")]
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private CinemachineCamera cam;
 
     private Vector3 currMovement;
 
-    private float cameraVertRotation;
-    
     void Start()
     {
         //see noi evil see no mousey comrade
@@ -44,20 +37,5 @@ public class PlayerMovementController : MonoBehaviour
             currMovement.Normalize();
             characterController.Move(currMovement * Time.deltaTime * speed);
         }
-    }
-
-    void HandleCameraRotation()
-    {
-        float mouseX = 0f;
-        float mouseY = 0f;
-        
-        //left right
-        transform.Rotate(0f, mouseX, 0f);
-        //up down
-        cameraVertRotation = Mathf.Clamp(cameraVertRotation - mouseY, -vertLimit, vertLimit);
-        cam.transform.localRotation = Quaternion.Euler(cameraVertRotation, 0f, 0f);
-
-
-        //TODO add input mapping for cam and assign to mouseX and MouseY
     }
 }
