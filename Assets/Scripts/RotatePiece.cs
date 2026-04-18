652da8d7cf8429521b8f6325a327fb11bcb60cf6
justness <vanessa.chu@mail.mcgill.ca>
2026-04-18 12:28:32 -0400

    Add pieces ticking down after delay

--- Assets/Scripts/RotatePiece.cs
+++ Assets/Scripts/RotatePiece.cs
@@ -1,24 +1,27 @@
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RotatePiece : MonoBehaviour
 {
-    RectTransform rt;
+    Transform pieceTransform;
     InputAction puzzleAction;
-    public RectTransform circle;
+    int originalPosition = 0;
+    public Transform circle;
     Vector2 circleOrigin;
     public bool leftControls = true;
+    float timer = 5;
+    bool resetting = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rt = gameObject.GetComponent<RectTransform>();
+        pieceTransform = gameObject.GetComponent<Transform>();
         if (leftControls)
             puzzleAction = InputSystem.actions.FindAction("PuzzleLeft");
         else puzzleAction = InputSystem.actions.FindAction("PuzzleRight");
-        circleOrigin = circle.TransformPoint(circle.rect.center);
+        circleOrigin = circle.position;
     }
 
     // Update is called once per frame
@@ -26,8 +29,42 @@ public class RotatePiece : MonoBehaviour
     {
         if (puzzleAction.WasPerformedThisFrame())
         {
-            float neg = -puzzleAction.ReadValue<float>();
-            rt.RotateAround(circleOrigin, Vector3.forward, 22.5f * neg);
+            timer = 5; // Reset timer
+            resetting = false;
+            float neg = puzzleAction.ReadValue<float>();
+            originalPosition += (int)neg;
+            pieceTransform.RotateAround(circleOrigin, Vector3.up, 22.5f * neg);
         }
+        else if (timer <= 0 && !resetting)
+        {
+            StartCoroutine(Reset());
+        }
+        else if (!resetting)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator Reset()
+    {
+        resetting = true;
+        if (timer > 0)
+        {
+            yield return null;
+        }
+        while (originalPosition > 0)
+        {
+            pieceTransform.RotateAround(circleOrigin, Vector3.up, -22.5f);
+            originalPosition--;
+            yield return new WaitForSeconds(.75f);
+        }
+        while (originalPosition < 0)
+        {
+            pieceTransform.RotateAround(circleOrigin, Vector3.up, 22.5f);
+            originalPosition++;
+            yield return new WaitForSeconds(.75f);
+        }
+        resetting = false;
+        yield return null;
     }
 }
