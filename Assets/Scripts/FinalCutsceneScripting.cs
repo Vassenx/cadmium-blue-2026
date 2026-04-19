5e3f77ebdf9d5e24148a3cf67004bfdb32a2ea24
justness <vanessa.chu@mail.mcgill.ca>
2026-04-19 04:54:54 -0400

    place cutscene elements

---
+++ Assets/Scripts/FinalCutsceneScripting.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FinalCutsceneScripting : MonoBehaviour
+{
+    public GameObject spear1;
+    public GameObject spear2;
+    public GameObject spear3;
+    public GameObject wolfPastor;
+    public GameObject flock;
+    // TODO: Audio and music
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
