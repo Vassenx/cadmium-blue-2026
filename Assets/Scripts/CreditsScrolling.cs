cd26c90af4ff1cf5311793ce0562933335f74ec4
mr-fufu <monkeymanp8@gmail.com>
2026-04-19 15:35:38 -0400

    Credits

--- Assets/Scripts/CreditsScrolling.cs
+++ Assets/Scripts/CreditsScrolling.cs
@@ -10,33 +10,62 @@ public class CreditsScrolling : MonoBehaviour
     public GameObject sheepPile;
     public GameObject title;
     public AudioSource audioSource;
+    public Transform[] allSheep;
 
     void Start()
     {
+        allSheep = new Transform[sheep.transform.childCount];
+
+        for (int sheepCount = 0; sheepCount < sheep.transform.childCount; sheepCount++)
+        {
+            allSheep[sheepCount] = sheep.transform.GetChild(sheepCount);
+        }
+
         StartCoroutine(Scroll());
     }
 
     IEnumerator Scroll()
     {
-        for (int i = 160; i > 0; i--)
+        for (int i = 165; i > 0; i--)
         {
-            // TODO: Change this to the wolf
-            candle.transform.position -= new Vector3(0, 0.08f, 0);
-            // Rotates as falling
-            candle.transform.Rotate(new Vector3(-0.8f, 0, 0.5f));
+            // DONE : Changed to wolf
+            wolf.transform.position -= new Vector3(0, 0.1f, 0);
+            wolf.transform.Rotate(new Vector3(0.8f, 0.2f, -0.5f));
             yield return new WaitForSeconds(0.05f);
         }
-        yield return new WaitForSeconds(1);
+
         audioSource.Play();
+        yield return new WaitForSeconds(2);
+
         // Text and sheep rain scrolling
-        for (int i = 1200; i > 0; i--)
+        for (int i = 700; i > 0; i--)
         {
-            text.GetComponent<RectTransform>().anchoredPosition += new Vector2(0, 2);
+            text.GetComponent<RectTransform>().anchoredPosition += new Vector2(0, 3.5f);
             sheep.transform.position -= new Vector3(0, 0.06f, 0);
+            foreach(Transform individualSheep in allSheep)
+            {
+                individualSheep.Rotate(new Vector3(0f, 0.2f, -0.5f));
+            }
             yield return new WaitForSeconds(0.05f);
         }
-        // TODO: candle falling
-        // TODO: sheep pile up
+
+        yield return new WaitForSeconds(1);
+
+        for (int i = 175; i > 0; i--)
+        {
+            candle.transform.position -= new Vector3(0, 0.08f, 0);
+            sheepPile.transform.position += new Vector3(0, 0.05f, 0);
+            candle.transform.Rotate(new Vector3(0, 0.2f, 0.4f));
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        for (int i = 180; i > 0; i--)
+        {
+            candle.transform.position += new Vector3(0, 0.05f, 0);
+            sheepPile.transform.position += new Vector3(0, 0.05f, 0);
+            yield return new WaitForSeconds(0.05f);
+        }
+
         // TODO: title fade in
         yield return null;
     }
